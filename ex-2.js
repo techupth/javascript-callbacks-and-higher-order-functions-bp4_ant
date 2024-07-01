ad061a245d1167b4393bbeb312582097dae35021
konthron gongmanee <konthron.ant@gmail.com>
2024-07-01 10:45:56 +0700

    feat: callbacks and higher order funtion finish

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,17 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  
+  let count = 0;
+  for (let i = 0 ; i < array.length ; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+  return count >= 5;
+}
+function checkGradeOver70(score) {
+  return score > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +19,10 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkGradeOver70);
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
